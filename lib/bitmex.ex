2866744b5e50fa7a3b43053c24fed927d7edfd4d
Ming Qu <qucool@gmail.com>
2017-01-30 21:00:15 +0800

    Initial commit

---
+++ lib/bitmex.ex
@@ -0,0 +1,18 @@
+defmodule Bitmex do
+  @moduledoc """
+  Documentation for Bitmex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bitmex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bitmex_test.exs
@@ -0,0 +1,8 @@
+defmodule BitmexTest do
+  use ExUnit.Case
+  doctest Bitmex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
