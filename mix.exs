58e643e8d5b34b36571f0ab5d927cd6a7760deac
Ming Qu <qucool@gmail.com>
2017-10-01 02:07:11 +0800

    Bump version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bitmex.Mixfile do
 
   def project do
     [app: :bitmex,
-     version: "0.1.0",
+     version: "0.1.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
