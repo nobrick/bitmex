208254d62629d6ee7b6cf6cbad482dc75f1443e4
Ming Qu <qucool@gmail.com>
2017-11-15 19:56:56 +0800

    Add Bitmex.Websocket.Parser to supervisor tree

--- lib/bitmex/application.ex
+++ lib/bitmex/application.ex
@@ -7,11 +7,14 @@ defmodule Bitmex.Application do
     import Supervisor.Spec, warn: false
 
     children = [
+      worker(Bitmex.Websocket.Parser, [[name: Bitmex.Websocket.Parser]],
+             max_restarts: :infinity, restart: :permanent),
+
       worker(Bitmex.Rest.Client, [[name: Bitmex.Rest.Client]],
-             max_restarts: Infinity),
+             max_restarts: :infinity, restart: :permanent),
 
       worker(Bitmex.Rest.RateLimiter, [[name: Bitmex.Rest.RateLimiter]],
-             max_restarts: Infinity)
+             max_restarts: :infinity, restart: :permanent)
     ]
 
     opts = [strategy: :one_for_one, name: Bitmex.Supervisor]
