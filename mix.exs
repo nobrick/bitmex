e2e0b90a16ec309d348e86cc68c3e7f3718a77ce
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-11-21 18:34:01 -0800

    Add integration test for order book

--- mix.exs
+++ mix.exs
@@ -2,36 +2,36 @@ defmodule Bitmex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bitmex,
-     version: "0.2.3",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: "BitMEX client library for Elixir",
-     source_url: "https://github.com/nobrick/bitmex",
-     homepage_url: "https://github.com/nobrick/bitmex",
-     package: package(),
-     deps: deps()]
+    [
+      app: :bitmex,
+      version: "0.2.3",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: "BitMEX client library for Elixir",
+      source_url: "https://github.com/nobrick/bitmex",
+      homepage_url: "https://github.com/nobrick/bitmex",
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
-    [extra_applications: [:logger],
-     mod: {Bitmex.Application, []}]
+    [extra_applications: [:logger], mod: {Bitmex.Application, []}]
   end
 
   defp deps do
-    [{:ex_doc, ">= 0.0.0", only: :dev},
-     {:websocket_client, "~> 1.2.1"},
-     {:websockex, "~> 0.4.0"},
-     {:poison, "~> 3.1.0"},
-     {:httpoison, "~> 1.0"}]
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:websocket_client, "~> 1.2.1"},
+      {:websockex, "~> 0.4.0"},
+      {:poison, "~> 3.1.0"},
+      {:httpoison, "~> 1.0"},
+      {:exvcr, "~> 0.10.3", only: [:dev, :test]}
+    ]
   end
 
   defp package do
-    [name: :bitmex,
-     maintainers: ["Ming Qu"],
-     licenses: ["MIT"],
-     links: %{}
-    ]
+    [name: :bitmex, maintainers: ["Ming Qu"], licenses: ["MIT"], links: %{}]
   end
 end
