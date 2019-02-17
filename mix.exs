cad5c16eba2cdfc73ad8fe7145892526eeab7ec4
Ming Qu <qucool@gmail.com>
2019-02-17 22:50:19 +0800

    Bump bitmex version to 0.2.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bitmex.Mixfile do
   def project do
     [
       app: :bitmex,
-      version: "0.2.4",
+      version: "0.2.5",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
